697510e475367320def42e555eb9efa512c14028
Thomas M <thomasm6m6@gmail.com>
2025-10-03 03:29:38 -0700

    electrobun!

---
+++ packages/frontend-native/electrobun.config.ts
@@ -0,0 +1,28 @@
+export default {
+    app: {
+        name: "cua",
+        identifier: "cua.vtom.net",
+        version: "0.0.1",
+    },
+    build: {
+        views: {
+            mainview: {
+                entrypoint: "src/mainview/index.ts",
+                external: [],
+            },
+        },
+        copy: {
+            "src/mainview/index.html": "views/mainview/index.html",
+            "src/mainview/index.css": "views/mainview/index.css",
+        },
+        mac: {
+            bundleCEF: false,
+        },
+        linux: {
+            bundleCEF: false,
+        },
+        win: {
+            bundleCEF: false,
+        },
+    },
+};
