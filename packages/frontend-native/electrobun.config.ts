849d4ad6fb4b78198a13c5e464969a09ea57c643
Thomas M <thomasm6m6@gmail.com>
2025-10-04 07:10:08 -0700

    frontend-native sort of works

--- packages/frontend-native/electrobun.config.ts
+++ packages/frontend-native/electrobun.config.ts
@@ -8,13 +8,17 @@ export default {
         views: {
             mainview: {
                 entrypoint: "src/mainview/index.ts",
+                // entrypoint: "dist/index.html",
                 external: [],
             },
         },
         copy: {
-            "src/mainview/index.html": "views/mainview/index.html",
-            "src/mainview/index.css": "views/mainview/index.css",
+            "dist": "views/mainview",
         },
+        // copy: {
+        //     "src/mainview/index.html": "views/mainview/index.html",
+        //     "src/mainview/index.css": "views/mainview/index.css",
+        // },
         mac: {
             bundleCEF: false,
         },
--- packages/frontend-native/src/mainview/index.ts
+++ packages/frontend-native/src/mainview/index.ts
@@ -1,1 +1,1 @@
-console.log("Hello Electrobun view loaded!");
+console.log("???");
