0379c08e5cba8b4a954ea1e45c37c423b6fab0cc
Thomas M <thomasm6m6@gmail.com>
2025-10-07 16:54:20 -0700

    readd open tool

--- packages/frontend-native/src/preload/index.ts
+++ packages/frontend-native/src/preload/index.ts
@@ -2,5 +2,6 @@ import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('electronAPI', {
   // Example IPC
-  sendMessage: (msg: string) => ipcRenderer.send('message', msg)
+  sendMessage: (msg: string) => ipcRenderer.send('message', msg),
+  openTool: (data: any) => ipcRenderer.invoke('open-tool', data),
 });
